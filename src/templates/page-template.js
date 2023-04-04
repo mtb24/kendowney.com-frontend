dd2ab67d78bc2e75473642f04d16547130249415
Ken Downey <ken@kendowney.com>
2023-04-04 16:46:56 -0700

    add page template

---
+++ src/templates/page-template.js
@@ -0,0 +1,30 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Seo from "../components/seo"
+import Layout from "../components/layout"
+
+export const query = graphql`
+    query($id: String!) {
+        wpPage(id: { eq: $id }) {
+            title
+            content
+        }
+    }
+`
+
+const PageTemplate = props => (
+    <Layout>
+        <h1>{props.data.wpPage.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: props.data.wpPage.content }} />
+        <pre>{JSON.stringify(props, null, 2)}</pre>
+    </Layout>
+)
+
+/**
+ * Head export to define metadata for the page
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
+ */
+export const Head = () => <Seo title="Home" />
+
+export default PageTemplate
