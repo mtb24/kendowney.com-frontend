347cf26befe64683914e11b4b995ccc14ebbfdf1
Ken Downey <ken@kendowney.com>
2023-04-06 22:01:02 -0700

    move components into files

---
+++ src/components/Main.js
@@ -0,0 +1,31 @@
+import * as React from "react"
+import styled from "styled-components"
+
+const Main = ({children}) => (
+    <StyledMain>{children}</StyledMain>
+)
+
+const StyledMain = styled.main`
+  padding: var(--space-3) var(--space-2);
+  overflow-y: scroll;
+
+  h1 {
+    text-align: center;
+  }
+  
+  div figure {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 0;
+    padding: 1rem;
+    
+    ul {
+      list-style: none;
+      margin: 0;
+      padding: 0;
+    }
+  }
+`
+
+export default Main
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,10 @@
 import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
-import styled from "styled-components"
-
-
 import Header from "./header"
 import "./layout.css"
 import Nav from "./nav"
+import Main from "./Main"
+import Footer from "./Footer"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -31,42 +30,10 @@ const Layout = ({ children }) => {
     <>
       <Header siteTitle={data.allSite.nodes[0]?.siteMetadata?.title || `Title`} />
       <Nav links={data.allWpPage.nodes} />
-      <StyledMain>{children}</StyledMain>
-      <StyledFooter>
-        © {new Date().getFullYear()} &middot; Ken Downey
-      </StyledFooter>
+      <Main>{children}</Main>
+      <Footer />
     </>
   )
 }
-
-const StyledMain = styled.main`
-  padding: var(--space-3) var(--space-2);
-  overflow-y: scroll;
-
-  h1 {
-    text-align: center;
-  }
-  
-  div figure {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin: 0;
-    padding: 1rem;
-    
-    ul {
-      list-style: none;
-      margin: 0;
-      padding: 0;
-    }
-  }
-`
-const StyledFooter = styled.footer`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: var(--font-sm);
-  background-color: var(--color-header-background);
-`
 
 export default Layout
