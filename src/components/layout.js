2d01f50ae7d0b8ec4ca1b38a18d3e364c399c192
Ken Downey <ken@kendowney.com>
2023-04-06 22:37:40 -0700

    restore filenames

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,10 @@
 import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
-import Header from "./Header"
+import Header from "./header"
 import "./layout.css"
-import Nav from "./Nav"
-import Main from "./Main"
-import Footer from "./Footer"
+import Nav from "./nav"
+import Main from "./main"
+import Footer from "./footer"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
--- src/templates/page-template.js
+++ src/templates/page-template.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { graphql } from "gatsby"
-import Seo from "../components/Seo"
+import Seo from "../components/seo"
 import Layout from "../components/layout"
 import styled from "styled-components"
 
