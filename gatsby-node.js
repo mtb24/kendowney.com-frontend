f1121537d99d910de2ec92b0fc42509d9983f01d
Ken Downey <ken@kendowney.com>
2023-04-08 15:39:18 -0700

    add app path

--- gatsby-node.js
+++ gatsby-node.js
@@ -34,3 +34,15 @@ exports.createPages = async ({ actions, graphql }) => {
     })
   })
 }
+
+exports.onCreatePage = async ({ page, actions }) => {
+  const { createPage } = actions
+
+  // page.matchPath is a special key that's used for matching pages
+  // only on the client.
+  if (page.path.match(/^\/app/)) {
+    page.matchPath = "/app/*"
+
+    createPage(page)
+  }
+}
