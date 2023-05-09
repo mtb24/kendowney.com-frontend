c9ea664fe70f3bb9a1aa0a96eff490bf8168dd25
Ken Downey <ken@kendowney.com>
2023-05-09 09:18:37 -0700

    feature:(add storybook)

---
+++ .storybook/main.js
@@ -0,0 +1,19 @@
+const React = require("react");
+
+/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+const config = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
+  },
+  core: {
+    builder: "webpack5",
+  },
+}
+export default config
---
+++ src/stories/Loading.stories.js
@@ -0,0 +1,28 @@
+import Loading from '../components/loading'
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+export default {
+  title: 'Components/Loading',
+  component: Loading,
+  tags: ['autodocs'],
+};
+
+const Template = args => <Loading {...args} />;
+
+export const Blue = Template.bind({});
+Blue.args = {
+    theme: {
+        width: '200px',
+        height: '200px',
+        dotColor: '#0047ab',
+    },
+};
+
+export const Red = Template.bind({});
+Red.args = {
+    theme: {
+        width: '200px',
+        height: '200px',
+        dotColor: 'red',
+    },
+};
