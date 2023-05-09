48db071b88f7e4f294f93603d48616c0e54fdd97
Ken Downey <ken@kendowney.com>
2023-05-09 11:01:09 -0700

    fix:(storybook) update config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,19 +1,34 @@
-const React = require("react");
-
-/** @type { import('@storybook/react-webpack5').StorybookConfig } */
-const config = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-  ],
+module.exports = {
+  stories: ["../src/**/*.stories.{js,jsx}"],
+  addons: ["@storybook/addon-docs", "@storybook/addon-actions"],
+  webpackFinal: async config => {
+    config.module.rules.push({
+      // Config for js and jsx files
+      test: /\.(js|jsx)$/,
+      use: [{
+        loader: require.resolve("babel-loader"),
+        options: {
+          presets: [
+          // use @babel/preset-react for JSX and env (instead of staged presets)
+          require.resolve("@babel/preset-react"), require.resolve("@babel/preset-env")],
+          plugins: [
+          // use @babel/plugin-proposal-class-properties for class arrow functions
+          require.resolve("@babel/plugin-proposal-class-properties"),
+          // use babel-plugin-remove-graphql-queries to remove static queries from components when rendering in storybook
+          require.resolve("babel-plugin-remove-graphql-queries"),
+          // use babel-plugin-react-docgen to ensure PropTables still appear
+          require.resolve("babel-plugin-react-docgen")]
+        }
+      }],
+      exclude: [/node_modules\/(?!(gatsby)\/)/]
+    });
+    return config;
+  },
   framework: {
     name: "@storybook/react-webpack5",
-    options: {},
+    options: {}
   },
-  core: {
-    builder: "webpack5",
-  },
-}
-export default config
+  docs: {
+    autodocs: true
+  }
+};
